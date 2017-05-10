f3a46110625eb1b6eaeec658aec48eac26b86f02
Pane <odalovicpane12@gmail.com>
2017-05-10 17:04:36 +0200

    pobednik

--- src/gui/JSpiloviPanel.java
+++ src/gui/JSpiloviPanel.java
@@ -2,9 +2,13 @@ package gui;
 
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+
+import java.awt.Color;
 import java.awt.Component;
 import javax.swing.Box;
+import java.awt.GridLayout;
 
 public class JSpiloviPanel extends JPanel {
 
@@ -12,7 +16,15 @@ public class JSpiloviPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public JSpiloviPanel() {
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		setLayout(new GridLayout(2, 1, 0, 0));
+		ImageIcon image=new ImageIcon("pozadina.jpg");
+		JButton goreKarte = new JButton(image);
+		goreKarte.setBackground(Color.blue);
+		add(goreKarte);
+		
+		JButton doleKarte = new JButton(image);
+		doleKarte.setBackground(Color.blue);
+		add(doleKarte);
 
 	}
 
