8a665382c956bccb2085bf1829a54676ffdf35a3
Pane <odalovicpane12@gmail.com>
2017-05-17 18:33:21 +0200

    dijalog

--- src/gui/DialogKlijentServer.java
+++ src/gui/DialogKlijentServer.java
@@ -5,8 +5,6 @@ import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.rmi.NotBoundException;
 
 import javax.swing.Box;
